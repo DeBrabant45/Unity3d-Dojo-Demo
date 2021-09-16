667af787d292e5faf54e464b89992b11d5ff219c
Aaron <DeBrabant45@users.noreply.github.com>
2021-09-16 16:01:38 -0400

    Adding in pause menu and controls menu

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -98,7 +98,7 @@ namespace AD.Player
             if (_animation.AnimatorService.GetAnimationBool("IsUsingRootMotion") != false)
             {
                 Vector3 animationDelta = _animation.AnimatorService.GetAnimationDeltaPosition();
-                _characterController.Move(animationDelta * _movementSpeed * Time.deltaTime * 35);
+                _characterController.Move(animationDelta * _movementSpeed * Time.deltaTime * 15);
             }
             else
             {
