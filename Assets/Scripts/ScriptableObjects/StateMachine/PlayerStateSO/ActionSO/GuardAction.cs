d1917854f626f3ff6b96bbeacc928061250ecb2f
Aaron <DeBrabant45@users.noreply.github.com>
2021-09-21 19:40:32 -0400

    Refactoring Block into Guard and Unguard class

---
+++ Assets/Scripts/ScriptableObjects/StateMachine/PlayerStateSO/ActionSO/GuardAction.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace AD.StateMachine.Player
+{
+    [CreateAssetMenu(menuName = "StateMachine/Player/Actions/Guard")]
+    public class GuardAction : PlayerAction
+    {
+        public override void Act(PlayerStateController controller)
+        {
+            if (controller.InputFromPlayer.IsSecondaryActionPressed() != false 
+                & !controller.Animations.AnimatorService.GetAnimationBool("IsInteracting"))
+            {
+                Guard(controller);
+            }
+        }
+
+        private void Guard(PlayerStateController controller)
+        {
+            if(!controller.BlockAttack.IsBlocking)
+            {
+                controller.BlockAttack.IsBlocking = true;
+                controller.Animations.AnimatorService.SetBoolForAnimation(controller.Weapon.BlockStanceAnimation, true);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/StateMachine/PlayerStateSO/ActionSO/UnguardAction.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace AD.StateMachine.Player
+{
+    [CreateAssetMenu(menuName = "StateMachine/Player/Actions/Unguard")]
+    public class UnguardAction : PlayerAction
+    {
+        public override void Act(PlayerStateController controller)
+        {
+            Unguard(controller);
+        }
+
+        private void Unguard(PlayerStateController controller)
+        {
+            if (controller.InputFromPlayer.IsSecondaryActionPressed() == false & controller.BlockAttack.IsBlocking)
+            {
+                controller.BlockAttack.IsBlocking = false;
+                controller.Animations.AnimatorService.SetBoolForAnimation(controller.Weapon.BlockStanceAnimation, false);
+            }
+        }
+    }
+}
