82fb10bc66b6721cb3e0eb69f48b2cef58a24023
Aaron <DeBrabant45@users.noreply.github.com>
2021-09-30 19:32:52 -0400

    Adding in Player guard/unguard actions and state

---
+++ Assets/Scripts/ScriptableObjects/StateMachine/PlayerStateSO/DecisionSO/GuardDecision.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace AD.StateMachine.Player
+{
+    [CreateAssetMenu(menuName = "StateMachine/Player/Decisions/Guard")]
+    public class GuardDecision : PlayerDecision
+    {
+        public override bool Decide(PlayerStateController controller)
+        {
+            return controller.InputFromPlayer.IsSecondaryActionPressed() != false
+                & !controller.Animations.AnimatorService.GetAnimationBool("IsInteracting");
+        }
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/StateMachine/PlayerStateSO/DecisionSO/UnguardDecision.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace AD.StateMachine.Player
+{
+    [CreateAssetMenu(menuName = "StateMachine/Player/Decisions/Unguard")]
+    public class UnguardDecision : PlayerDecision
+    {
+        public override bool Decide(PlayerStateController controller)
+        {
+            return controller.BlockAttack.IsBlocking == false && controller.BaseStats.Posture.IsBroken == false;
+        }
+    }
+}
