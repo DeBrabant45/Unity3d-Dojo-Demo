ebb546476b936a3a78efe6bfab8b446b589df29d
Aaron <DeBrabant45@users.noreply.github.com>
2021-10-11 16:40:47 -0400

    Refactoring PlayerMovment to have cleaner names/methods and removing unused code

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -12,12 +12,11 @@ namespace AD.Player
 
         private CharacterController _characterController;
         private Vector3 _moveDirection = Vector3.zero;
+        private Vector2 _input;
         private float _desiredRotationAngle = 0;
         private IAnimation _animation;
-        private int _inputVerticalDirection = 0;
-        private bool _temporaryMovementTriggered = false;
-        private Vector2 _input;
         private Transform _mainCameraTransform;
+        private bool _isMovementHorizontal = false;
         private bool _isMoving = false;
 
         private void Start()
@@ -34,74 +33,59 @@ namespace AD.Player
             {
                 if (input.y != 0)
                 {
-                    _temporaryMovementTriggered = false;
-                    if (input.y > 0)
-                    {
-                        //while player is moving forward
-                        _inputVerticalDirection = Mathf.CeilToInt(input.y);
-                    }
-                    else
-                    {
-                        //while player is moving backward
-                        _inputVerticalDirection = Mathf.FloorToInt(input.y);
-                    }
-                    _moveDirection = input.y * transform.forward;
-                    _isMoving = true;
+                    _isMovementHorizontal = false;
+                    SetMoveDirectionToYInput(input);
+                }
+                else if (input.x != 0)
+                {
+                    _isMovementHorizontal = true;
+                    SetMovementDirectionToXInput(input);
                 }
                 else
                 {
-                    if (input.x != 0)
-                    {
-                        if (_temporaryMovementTriggered == false)
-                        {
-                            _temporaryMovementTriggered = true;
-                        }
-                        _inputVerticalDirection = 1;
-                        _moveDirection = new Vector3(input.x, 0f, 0f);
-                        _isMoving = true;
-                    }
-                    else
-                    {
-                        StopMovement();
-                    }
+                    StopMovement();
                 }
             }
         }
 
+        private void SetMovementDirectionToXInput(Vector2 input)
+        {
+            _moveDirection = new Vector3(input.x, 0f, 0f);
+            _isMoving = true;
+        }
+
+        private void SetMoveDirectionToYInput(Vector2 input)
+        {
+            _moveDirection = input.y * transform.forward;
+            _isMoving = true;
+        }
+
         private void Update()
+        {
+            SetCharacterMovementDirection();
+            MoveCharacter();
+        }
+
+        private void SetCharacterMovementDirection()
         {
             if (CharacterIsGrounded())
             {
                 if (_isMoving)
                 {
-                    if (_temporaryMovementTriggered == false)
+                    if (_isMovementHorizontal == false)
                     {
-                        RotateAgent();
+                        RotateCharacterBasedOnVeritcalInput();
                     }
                     else
                     {
-                        float targetAngle = Mathf.Atan2(_moveDirection.x, _moveDirection.z) * Mathf.Rad2Deg + _mainCameraTransform.eulerAngles.y;
-                        Vector3 move = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-                        _moveDirection = move;
+                        SetMovementDirectionBasedOnHorizontalInput();
                     }
-                    _animation.SetAnimationFloat("InputX", _animation.SetAnimationFloatInput(_input.x, _animation.GetAnimationFloat("InputX")));
-                    _animation.SetAnimationFloat("InputY", _animation.SetAnimationFloatInput(_input.y, _animation.GetAnimationFloat("InputY")));
+                    SetMovementAnimationBasedOnXYInput();
                 }
-            }
-
-            if (_animation.GetAnimationBool("IsUsingRootMotion") != false)
-            {
-                Vector3 animationDelta = _animation.GetAnimationDeltaPosition();
-                _characterController.Move(animationDelta * _movementSpeed * Time.deltaTime * 20);
-            }
-            else
-            {
-                _moveDirection.y -= _gravity;
-                _characterController.Move(_moveDirection * _movementSpeed * Time.deltaTime);
             }
         }
 
-        private void RotateAgent()
+        private void RotateCharacterBasedOnVeritcalInput()
         {
             //Rotates player based on users joystick direction
             if (_desiredRotationAngle > _angleRotationThreshold || _desiredRotationAngle < -_angleRotationThreshold)
@@ -110,9 +94,46 @@ namespace AD.Player
             }
         }
 
+        private void SetMovementDirectionBasedOnHorizontalInput()
+        {
+            float targetAngle = Mathf.Atan2(_moveDirection.x, _moveDirection.z) * Mathf.Rad2Deg + _mainCameraTransform.eulerAngles.y;
+            Vector3 move = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            _moveDirection = move;
+        }
+
+        private void SetMovementAnimationBasedOnXYInput()
+        {
+            _animation.SetAnimationFloat("InputX", _animation.SetAnimationFloatInput(_input.x, _animation.GetAnimationFloat("InputX")));
+            _animation.SetAnimationFloat("InputY", _animation.SetAnimationFloatInput(_input.y, _animation.GetAnimationFloat("InputY")));
+        }
+
+        private void MoveCharacter()
+        {
+            if (_animation.GetAnimationBool("IsUsingRootMotion") != false)
+            {
+                MoveCharacterByRootMotionPosition();
+            }
+            else
+            {
+                MoveCharacterByMoveDirectionPosition();
+            }
+        }
+
+        private void MoveCharacterByMoveDirectionPosition()
+        {
+            _moveDirection.y -= _gravity;
+            _characterController.Move(_moveDirection * _movementSpeed * Time.deltaTime);
+        }
+
+        private void MoveCharacterByRootMotionPosition()
+        {
+            Vector3 animationDelta = _animation.GetAnimationDeltaPosition();
+            _characterController.Move(animationDelta * _movementSpeed * Time.deltaTime * 20);
+        }
+
         public void HandleMovementDirection(Vector3 input)
         {
-            if (_temporaryMovementTriggered)
+            if (_isMovementHorizontal)
             {
                 return;
             }
@@ -124,12 +145,11 @@ namespace AD.Player
             }
         }
 
-        public void StopMovement()
+        private void StopMovement()
         {
             _isMoving = false;
             _moveDirection = Vector3.zero;
             _desiredRotationAngle = 0;
-            _inputVerticalDirection = 0;
             _animation.SetAnimationFloat("InputX", _animation.LowerAnimationFloatInputToZero(_animation.GetAnimationFloat("InputX")));
             _animation.SetAnimationFloat("InputY", _animation.LowerAnimationFloatInputToZero(_animation.GetAnimationFloat("InputY")));
         }
