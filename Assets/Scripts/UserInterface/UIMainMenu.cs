e1e601ef8bea3455fd6bab507577ce6a049977b2
Aaron <DeBrabant45@users.noreply.github.com>
2021-10-25 20:37:46 -0400

    Adding in post processing effects for better visuals

--- Assets/Scripts/UserInterface/UIMainMenu.cs
+++ Assets/Scripts/UserInterface/UIMainMenu.cs
@@ -9,6 +9,7 @@ namespace AD.UI
         [SerializeField] private Button _playBtn;
         [SerializeField] private Button _optionsBtn;
         [SerializeField] private GameObject _optionsPanel;
+        [SerializeField] private AudioSource _playButtonClicked;
         private UINextSceneFade _nextSceneFade;
 
         private void Start()
@@ -25,6 +26,7 @@ namespace AD.UI
 
         private void OnPlayClicked()
         {
+            _playButtonClicked.Play();
             _nextSceneFade.FadeBackGround.enabled = true;
         }
     }
